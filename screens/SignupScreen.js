36848368c16470fe0fd3d32910221f0e90bfa954
Matt Hlavaty <mwhlavaty@gmail.com>
2024-04-26 16:58:47 -0700

    initialize firebase in project

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -26,30 +26,6 @@ export default function SignupScreen() {
       if (res.error) setError(res.error);
     }
   };
-
-  // return (
-  //   <View>
-  //     <Text>Sign Up</Text>
-  //     <TextInput
-  //       value={email}
-  //       placeholder="Email"
-  //       keyboardType="email-address"
-  //       onChange={(e) => setEmail(e.target.value)}
-  //     />
-  //     <TextInput
-  //       value={password}
-  //       secureTextEntry={true}
-  //       placeholder="Password"
-  //       onChange={(e) => setPassword(e.target.value)}
-  //     />
-  //     <TextInput
-  //       value={password2}
-  //       secureTextEntry={true}
-  //       placeholder="Reenter Password"
-  //     />
-  //     <TouchableOpacity onPress={handleSubmit}>Submit</TouchableOpacity>
-  //   </View>
-  // );
   
  return (
     <View style={styles.container}>
